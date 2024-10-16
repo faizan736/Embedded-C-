c24af12219ed19be42eda7de4b3d41325da77e95
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-10-16 18:16:33 +0530

    Create code.c

---
+++ FileGeneration /code.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // the stdio.h files consists of all function 
+
+
+int main(){
+    printf("Hello world ");
+    return 0;
+}
+
+// main.c ---> main.i ---> main.s ---> main.o
+// c program --> preprocessed file --> assembly language --> binary code 
